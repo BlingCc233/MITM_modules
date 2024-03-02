59eed74dc901b79740b9d796bcddc9c7bd7cfaf0
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2024-03-02 16:34:37 +0800

    Create legymrun.js

---
+++ legymrun.js
@@ -0,0 +1,29 @@
+/*
+乐健修改跑步数据
+（适用于UESTC）
+脚本版本： 1.0.1
+脚本作者：BlingCc
+更新时间：2024-03-02
+主页：blingcc.eu.org
+声明：此脚本仅供学习交流
+
+**************************
+
+QuantumultX:
+
+[rewrite_locala]
+^https?:\/\/cpes\.legym\.cn\/education\/activity\/simple\/attainability\/get https://raw.githubusercontent.com/BlingCc233/MITM_modules/main/legym.js
+
+[mitm] 
+
+hostname = cpes.legym.cn
+
+*******************************/
+
+var obj = JSON.parse($response.body)
+
+obj.data.radius = 6666;
+
+$done({
+  body: JSON.stringify(obj),
+})
