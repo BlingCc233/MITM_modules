20a8ca0ee9972eadbc965ccbd5ccdc645bffefb2
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2024-03-03 14:36:55 +0800

    Update prefix_legym.js

--- prefix_legym.js
+++ prefix_legym.js
@@ -1,8 +1,8 @@
 /*
 乐健修改跑步限制
-脚本版本： 1.0.1
+脚本版本： 1.0.2
 脚本作者：BlingCc
-更新时间：2024-03-02
+更新时间：2024-03-03
 主页：blingcc.eu.org
 声明：此脚本仅供学习交流
 
@@ -19,7 +19,7 @@ hostname = cpes.legym.cn
 
 *******************************/
 
-var obj = JSON.parse($response.body);
+var obj = JSON.parse(response.body);
 
 obj.data.averageSpeedStart = "0";
 obj.data.averageSpeedEnd = "8100000000";
@@ -28,6 +28,13 @@ obj.data.effectiveMileageStart = 0;
 obj.data.effectiveMileageEnd = 4.0;
 obj.data.dailyMileage = 50;
 obj.data.weeklyMileage = 50;
+// 设置每天的结束时间为24点
+obj.openTimes[0].dayEndTime = "86400000"; // 24 * 60 * 60 * 1000
+obj.weekOpenTimes[0].dayEndTime = "86400000"; // 24 * 60 * 60 * 1000
+
+$done({
+  body: JSON.stringify(obj),
+});
 
 $done({
   body: JSON.stringify(obj),
